2dbec8e5cf4cbe8ef9b75a2e55e42abf8afd99d3
Linton Mackereth <linton@lintons-mbp.home>
2017-07-16 11:21:47 +0100

    request file updated and successfully pulling Getting information from the API

---
+++ js/request.js
@@ -0,0 +1,14 @@
+var request = new XMLHttpRequest()
+
+request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=e14e76429c1dfbea37cce5b6efaab2e1')
+
+request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        var responseJSON = JSON.parse(this.response)
+        var main = responseJSON.main
+
+        console.log(main)
+    }
+}
+
+request.send()
